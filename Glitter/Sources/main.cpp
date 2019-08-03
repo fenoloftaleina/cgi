3fe67d1cf9e007607860f5f0fa8ad8a015d2358b
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2019-08-03 13:45:55 +0200

    Do nice things.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -9,7 +9,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main(int argc, char * argv[]) {
+int main() {
 
     // Load GLFW and Create a Window
     glfwInit();
@@ -29,16 +29,135 @@ int main(int argc, char * argv[]) {
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
     gladLoadGL();
-    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+    float vertices[] = {
+      0.5f,  0.5f, 0.0f,   0.1f, 0.2f, 0.3f, // top right
+      0.5f, -0.5f, 0.0f,   0.4f, 0.0f, 0.5f, // bottom right
+      -0.5f, -0.5f, 0.0f,  0.5f, 0.5f, 0.5f, // bottom left
+      -0.5f,  0.5f, 0.0f,  0.1f, 0.5f, 0.7f  // top left
+    };
+
+    unsigned int indices[] = {  // note that we start from 0!
+      0, 1, 3,   // first triangle
+      1, 2, 3    // second triangle
+    };
+
+    const char* vertexSource = R"glsl(
+      #version 330 core
+      layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
+      layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
+
+      uniform mat4 model;
+      uniform mat4 view;
+      uniform mat4 projection;
+
+      out vec3 ourColor; // output a color to the fragment shader
+
+      void main()
+      {
+          gl_Position = projection * view * model * vec4(aPos, 1.0);
+          ourColor = aColor; // set ourColor to the input color we got from the vertex data
+      }
+    )glsl";
+
+    const char* fragmentSource = R"glsl(
+      #version 330 core
+
+      in vec3 ourColor;
+      out vec4 outColor;
+
+      uniform float t;
+      uniform float shade;
+
+      void main()
+      {
+          outColor = vec4(ourColor * shade * pow(sin(t), 2.0) + 0.2, 1.0);
+      }
+    )glsl";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    char buffer[512];
+    int success;
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+      fprintf(stderr, "Vertex shader info log:\n%s\n", buffer);
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
+      fprintf(stderr, "Fragment shader info log:\n%s\n", buffer);
+    }
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+
+    unsigned int vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    int tUniform = glGetUniformLocation(shaderProgram, "t");
+    int shadeUniform = glGetUniformLocation(shaderProgram, "shade");
+    int modelUniform = glGetUniformLocation(shaderProgram, "model");
+    int viewUniform = glGetUniformLocation(shaderProgram, "view");
+    int projectionUniform = glGetUniformLocation(shaderProgram, "projection");
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+    glm::mat4 view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), mWidth / (float) mHeight, 0.1f, 100.0f);
+
+    float t;
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(mWindow, true);
 
+        t = glfwGetTime();
+
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glUniform1f(tUniform, t);
+        glUniform1f(shadeUniform, 0.9f);
+        glUniformMatrix4fv(modelUniform, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewUniform, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionUniform, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
