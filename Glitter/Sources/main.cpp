b3d41ee89dc03424b79e962302e8fd60619fc433
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2019-08-16 23:46:59 +0200

    Add blending.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -111,6 +111,10 @@ int main() {
     glEnable(GL_CULL_FACE);
 
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
     float vertices[] = {
       // -0.5f, -0.5f, -0.5f, 0.1f, 0.2f, 0.3f,
       // -0.5f,  0.5f, -0.5f, 0.1f, 0.5f, 0.7f,
